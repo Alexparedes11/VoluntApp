e99dc42043cc8469580f05b5ae5bfdebb69c6779
noelbalmis <noel.leon@iesdoctorbalmis.com>
2024-01-25 12:44:41 +0100

    Componente event card a medias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { TermsComponent } from './terms/terms.component';
+import { EventCardComponent } from './components/event-card/event-card.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'terms', component: TermsComponent }
+    { path: 'terms', component: TermsComponent },
 ];
