e337d0dd0d1937b8c3db4b23083e59cb3814456c
alejandro <alejandro.paredes@iesdoctorbalmis.com>
2024-01-29 10:12:56 +0100

    filtros sin terminar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { EventCardComponent } from '../components/event-card/event-card.component';
+import { EventFiltersComponent } from '../components/event-filters/event-filters.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent,EventCardComponent],
+  imports: [HeaderComponent,FooterComponent,EventCardComponent,EventFiltersComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
