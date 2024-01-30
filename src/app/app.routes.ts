47f956aa45d0c73698a4ae6876b7a19bc240416d
Saul Valcarcel Picapiedra <saul.valcarcel@iesdoctorbalmis.com>
2024-01-30 14:12:09 +0100

    Modificacion de header, footer y creacion de perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { TermsComponent } from './terms/terms.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -12,5 +13,6 @@ export const routes: Routes = [
     { path: 'terms', component: TermsComponent },
     { path: 'about', component: AboutComponent },
     { path: '', component: HomeComponent },
-    {path:'news',component: NewsComponent}
+    {path:'news',component: NewsComponent},
+    {path: 'profile', component: ProfileComponent}
 ];
