e682f7773dfa04de8f54d66e5cb3a5e583be2dd3
alejandro <alejandro.paredes@iesdoctorbalmis.com>
2024-01-30 09:16:20 +0100

    Update links in header and footer components

--- src/app/terms/terms.component.ts
+++ src/app/terms/terms.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../components/header/header.component";
+import { FooterComponent } from "../components/footer/footer.component";
 
 @Component({
   selector: 'app-terms',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './terms.component.html',
   styleUrl: './terms.component.scss'
 })
