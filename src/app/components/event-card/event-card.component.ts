6992a1b38bf8730937388e503f91a9e66001871f
noelbalmis <noel.leon@iesdoctorbalmis.com>
2024-01-31 09:20:04 +0100

    Tarjeta de evento recibe información real desde un array

--- src/app/components/event-card/event-card.component.ts
+++ src/app/components/event-card/event-card.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, numberAttribute } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-event-card',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './event-card.component.html',
   styleUrl: './event-card.component.scss'
 })
 export class EventCardComponent {
-
+  @Input() image: string | null = null;
+  @Input() title: string | null = null;
+  @Input() startDate: Date | null = null;
+  @Input() endDate: Date | null = null;
+  @Input() location: string | null = null;
+  @Input({transform: numberAttribute}) volunteers: Number = 1;
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,5 +12,60 @@ import { EventFiltersComponent } from '../components/event-filters/event-filters
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  events = [
+    {
+      id: 1,
+      image: '/assets/images/bosque.jpg',
+      title: 'Evento 1',
+      startDate: new Date(),
+      endDate: new Date(),
+      location: 'São Paulo',
+      volunteers: 5
+    },
+    {
+      id: 2,
+      image: '/assets/images/cascada.jpg',
+      title: 'Evento 2',
+      startDate: new Date(),
+      endDate: new Date(),
+      location: 'São Paulo',
+      volunteers: 5
+    },
+    {
+      id: 3,
+      image: '/assets/images/montana.jpg',
+      title: 'Evento 3',
+      startDate: new Date(),
+      endDate: new Date(),
+      location: 'São Paulo',
+      volunteers: 5
+    },
+    {
+      id: 4,
+      image: '/assets/images/rio.jpg',
+      title: 'Evento 4',
+      startDate: new Date(),
+      endDate: new Date(),
+      location: 'São Paulo',
+      volunteers: 5
+    },
+    {
+      id: 5,
+      image: '/assets/images/bosque.jpg',
+      title: 'Evento 5',
+      startDate: new Date(),
+      endDate: new Date(),
+      location: 'São Paulo',
+      volunteers: 5
+    },
+    {
+      id: 6,
+      image: '/assets/images/bannerprofile.jpg',
+      title: 'Evento 6',
+      startDate: new Date(),
+      endDate: new Date(),
+      location: 'São Paulo',
+      volunteers: 5
+    }
+  ];
 }
