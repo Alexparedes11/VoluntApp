ec67f064c5184d71d1cd710cd35387e4221182ac
Saul Valcarcel Picapiedra <saul.valcarcel@iesdoctorbalmis.com>
2024-01-31 11:58:57 +0100

    Filtros home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { EventCardComponent } from '../components/event-card/event-card.component';
 import { EventFiltersComponent } from '../components/event-filters/event-filters.component';
+import { FiltersBarComponent } from '../components/filters-bar/filters-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent,EventCardComponent,EventFiltersComponent],
+  imports: [HeaderComponent,FooterComponent,EventCardComponent,EventFiltersComponent, FiltersBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
