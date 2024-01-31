6ebde4516ac1dbcf251eb8764cc1c1b1594ab1d2
noelbalmis <noel.leon@iesdoctorbalmis.com>
2024-01-31 12:01:40 +0100

    Update app.config.ts and event.component.ts

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { Event } from '../models/Event';
+import { User } from '../models/User';
 
 @Component({
   selector: 'app-event',
@@ -11,4 +13,30 @@ import { FooterComponent } from '../components/footer/footer.component';
 })
 export class EventComponent {
 
+  @Input("id") eventId: Number | null = null;
+
+  // Usuario de ejemplo
+  userExample: User = {
+    id: 1,
+    name: "Pedro",
+    surname: "Martínez",
+    dni: "48776243X",
+    address: "Calle de la piruleta, 69",
+    email: "paco@gmail.com",
+    password: "paco",
+    profileImage: "/assets/images/bosque.jpg",
+    rol: "user"
+  };
+
+  // Aquí se obtendría el evento a partir de la id
+  event: Event = {
+    id: 1,
+    image: '/assets/images/mayores.jpg',
+    title: 'Informatica para mayores',
+    startDate: new Date(),
+    endDate: new Date(),
+    location: 'Alicante',
+    volunteers: 15,
+    createdBy: this.userExample
+  }
 }
