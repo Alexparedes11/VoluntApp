99ad8ce32b45bdfac7f31d553e25a20dfd334ab8
alejandro <alejandro.paredes@iesdoctorbalmis.com>
2024-02-01 10:18:22 +0100

    contact page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { EventCardComponent } from '../../components/event-card/event-card.component';
-import { EventFiltersComponent } from '../../components/event-filters/event-filters.component';
 import { FiltersBarComponent } from '../../components/filters-bar/filters-bar.component';
 import { exampleEvents } from '../../data/exampleEventList';
 import { Event } from "../../models/Event";
@@ -10,7 +9,7 @@ import { Event } from "../../models/Event";
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, EventCardComponent, EventFiltersComponent, FiltersBarComponent],
+  imports: [HeaderComponent, FooterComponent, EventCardComponent, FiltersBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
