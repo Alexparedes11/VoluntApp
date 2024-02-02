e7dea0ea5a561a80d7be06dbaef33a12aa627b26
noelbalmis <noel.leon@iesdoctorbalmis.com>
2024-02-02 12:39:54 +0100

    Refactor event-card component and home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { EventCardComponent } from '../../components/event-card/event-card.component';
 import { FiltersBarComponent } from '../../components/filters-bar/filters-bar.component';
+import { MainService } from '../../services/main.service';
+import { HttpClientModule } from '@angular/common/http';
+import { EventCardDTO } from '../../models/dto/EventCardDTO';
+import { Event } from '../../models/Event';
 import { exampleEvents } from '../../data/exampleEventList';
-import { Event } from "../../models/Event";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, EventCardComponent, FiltersBarComponent],
+  providers: [MainService],
+  imports: [HeaderComponent, FooterComponent, EventCardComponent, FiltersBarComponent, HttpClientModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  events: Event[] = exampleEvents;
+export class HomeComponent implements OnInit {
+  constructor(private mainService: MainService) { }
+  events: EventCardDTO[] = [];
+
+  ngOnInit(): void {
+    this.mainService.getEvents().subscribe(
+      (data) => {
+        this.events = data.content;
+      },
+      (error) => {
+        console.error('Error fetching events:', error);
+      }
+    );
+  }
 }
--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -1,16 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
 
-  private baseUrl: String = 'http://localhost:9000';
+  private baseUrl: String = 'http://10.100.24.1:9000';
 
   constructor(private http: HttpClient) { }
 
   createUser(data: any) {
     return this.http.post(`${this.baseUrl}/usuarios`, data)
+  }
+
+  logUser(data: any) {
+    return this.http.post(`${this.baseUrl}/login`, data)
+  }
+
+  getEvents() {
+    return this.http.get(`${this.baseUrl}/eventos`).pipe(
+      map((data: any) => {
+        return data;
+      })
+    );
   }
 }
