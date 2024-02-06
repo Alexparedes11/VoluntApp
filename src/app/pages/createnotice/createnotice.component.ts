17129511785175aba3f64042ebf9b243e07c1443
Saul Valcarcel Picapiedra <saul.valcarcel@iesdoctorbalmis.com>
2024-02-06 10:18:39 +0100

    Creada zona crear noticias

--- src/app/pages/createnotice/createnotice.component.ts
+++ src/app/pages/createnotice/createnotice.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-createnotice',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './createnotice.component.html',
   styleUrl: './createnotice.component.scss'
 })
