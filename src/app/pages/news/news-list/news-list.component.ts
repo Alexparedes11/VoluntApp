c8d92c9ce879baf2f806218274fd411822328e09
alejandro <alejandro.paredes@iesdoctorbalmis.com>
2024-02-07 08:36:05 +0100

    noticias mostradas desde la base datos

--- src/app/pages/news/news-list/news-list.component.ts
+++ src/app/pages/news/news-list/news-list.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../../../components/header/header.component";
 import { FooterComponent } from "../../../components/footer/footer.component";
 import { DatePipe } from '@angular/common';
-import { NewsDTO } from '../../../models/dto/NewsDTO';
+import { NewsService } from '../../../services/news.service';
+import { HeaderComponent } from "../../../components/header/header.component";
+import { Component } from "@angular/core";
+import { NewsDTO } from "../../../models/dto/NewsDTO";
 
 @Component({
   selector: 'app-news-list',
   standalone: true,
   templateUrl: './news-list.component.html',
   styleUrl: './news-list.component.scss',
+  providers: [NewsService, DatePipe],
   imports: [HeaderComponent, FooterComponent, DatePipe],
 })
 export class NewsListComponent {
+  constructor(private newsService: NewsService) { }
   news: NewsDTO[] = [];
+  ngOnInit(): void {
+    this.newsService.getNews().subscribe(
+      (data) => {
+        this.news = data.content;
+      }
+    );
+  }
 }
 
