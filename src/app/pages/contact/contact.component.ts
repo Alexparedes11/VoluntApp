d943eba0731cf58b07e107adb9c841a8708e1279
alejandro <alejandro.paredes@iesdoctorbalmis.com>
2024-02-08 13:43:52 +0100

    expediente en el contacto (ID)

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -4,11 +4,12 @@ import { ContactService } from '../../services/contact.service';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  providers: [ContactService],
+  providers: [ContactService, UserService],
   imports: [HeaderComponent, FooterComponent, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
@@ -21,13 +22,13 @@ export class ContactComponent {
     mensaje: new FormControl('')
   });
 
-  constructor(private contactService: ContactService) { }
+  constructor(private contactService: ContactService,private userService: UserService) { }
 
   hacerConsulta() {
     const consulta = {
       email: this.contactForm.value.email,
-      asunto: this.contactForm.value.nombre + ' ' + this.contactForm.value.apellidos,
-      mensaje: this.contactForm.value.mensaje
+      asunto: "ID usuario: "+this.userService.isLogged() ? this.userService.getUserIdFromToken().toString() : 'Consulta de invitado',
+      mensaje: this.contactForm.value.nombre + ' ' + this.contactForm.value.apellidos+"\n"+"Consulta: "+this.contactForm.value.mensaje
     };
     this.contactService.enviarConsulta(consulta).subscribe(response => {
       console.log(response);
